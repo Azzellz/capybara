64828be74aad846bf9945d403b534514577efe12
YuzuTea <flin00806@gmail.com>
2025-11-29 16:35:00 +0800

    feat: Updated transition effects for module switching

--- src/renderer/src/components/app/AppContent.vue
+++ src/renderer/src/components/app/AppContent.vue
@@ -5,11 +5,30 @@ import { NCard } from 'naive-ui'
 import { useGlobalStore } from '@renderer/stores/global'
 
 const globalStore = useGlobalStore()
+const components = {
+  client: ClientModule,
+  file: FileModule
+}
 </script>
 
 <template>
   <NCard class="shadow-[2px_2px_2px_2px_#999]" content-style="padding: 0px">
-    <ClientModule v-if="globalStore.currentModule === 'client'" />
-    <FileModule v-else-if="globalStore.currentModule === 'file'" />
+    <Transition name="fade" mode="out-in">
+      <KeepAlive>
+        <component :is="components[globalStore.currentModule]" />
+      </KeepAlive>
+    </Transition>
   </NCard>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
